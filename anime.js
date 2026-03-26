d035ee8a06c6f3f32aaba9604871cb8d6853c7e2
Thooms08 <thomaslevibaehaqi@gmail.com>
2026-03-26 08:22:56 +0700

    ubah copyright

--- anime.js
+++ anime.js
@@ -157,6 +157,11 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
+    const yearSpan = document.querySelector('.current-year');
+    if(yearSpan) {
+        yearSpan.textContent = new Date().getFullYear();
+    }
+
     // ==========================================
     // FITUR SHARE PORTFOLIO
     // ==========================================
